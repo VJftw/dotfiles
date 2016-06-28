2b72d7a06de8d2a2ba374ae55a391a76f673117b
VJ Patel <meetthevj@gmail.com>
2016-06-28 13:58:16 +0100

    fixed redirect curl download for powerline font

--- ubuntu/unity-setup.sh
+++ ubuntu/unity-setup.sh
@@ -64,7 +64,7 @@ echo ""
 echo "Installing Fantasque Sans Mono and a Powerline patched font"
 echo ""
 mkdir -p ~/.fonts
-curl https://gist.github.com/qrush/1595572/raw/6c453ddc959b93895ffbf4fd904e2ba60256c904/Menlo-Powerline.otf -o ~/.fonts/menlo-powerline.otf
+curl -L https://gist.github.com/qrush/1595572/raw/6c453ddc959b93895ffbf4fd904e2ba60256c904/Menlo-Powerline.otf -o ~/.fonts/menlo-powerline.otf
 curl -L -O https://github.com/belluzj/fantasque-sans/releases/download/v1.7.1/FantasqueSansMono.tar.gz
 tar -xvzf FantasqueSansMono.tar.gz
 mv *.ttf ~/.fonts/
