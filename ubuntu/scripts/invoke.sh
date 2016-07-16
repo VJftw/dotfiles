0c7eca815adbfa95f70c07fc9bb53fac5b72f7f4
VJ Patel <meetthevj@gmail.com>
2016-07-16 11:47:58 +0100

    Update invoke.sh

--- ubuntu/scripts/invoke.sh
+++ ubuntu/scripts/invoke.sh
@@ -3,7 +3,7 @@
 echo ""
 echo "Installing Pip, Invoke and invoke-docker-flow"
 echo ""
-sudo apt-get install python3-dev python3
+sudo apt-get install -y python3-dev python3
 curl -O -L https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 sudo pip install invoke-docker-flow
