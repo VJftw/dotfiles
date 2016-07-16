7ce560286611d28d13f7229bc883ea7325c2a3af
VJ Patel <meetthevj@gmail.com>
2016-07-16 11:54:41 +0100

    Update liquorix.sh

--- ubuntu/scripts/liquorix.sh
+++ ubuntu/scripts/liquorix.sh
@@ -6,8 +6,8 @@ echo ""
 
 echo 'deb http://mirror.unit193.net/liquorix sid main' | sudo tee --append /etc/apt/sources.list.d/liquorix.list
 sudo apt-get update -y
-sudo apt-get install liquorix-keyring
+sudo apt-get install -y liquorix-keyring
 sudo apt-get update -y
-sudo apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64
+sudo apt-get install -y linux-image-liquorix-amd64 linux-headers-liquorix-amd64
 
-sudo apt-get autoremove
+sudo apt-get autoremove -y
