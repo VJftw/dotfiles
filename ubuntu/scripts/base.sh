6ae59aa97bdf8f6692c8a4fa202ff81a67ace793
VJ Patel <meetthevj@gmail.com>
2016-10-24 19:16:18 +0100

    Update base.sh

--- ubuntu/scripts/base.sh
+++ ubuntu/scripts/base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ""
-echo "Installing curl, htop, terminator"
+echo "Installing curl, htop, terminator, vim"
 echo ""
-sudo apt-get -f install curl htop terminator apt-transport-https ca-certificates -y
+sudo apt-get -f install vim curl htop terminator apt-transport-https ca-certificates -y
 gsettings set org.gnome.desktop.default-applications.terminal exec 'terminator'
