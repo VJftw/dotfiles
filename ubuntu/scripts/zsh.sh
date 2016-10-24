2a365cab8332f08da478102b21f3f9b4514d1206
VJ Patel <meetthevj@gmail.com>
2016-10-24 18:47:28 +0100

    Update zsh.sh

--- ubuntu/scripts/zsh.sh
+++ ubuntu/scripts/zsh.sh
@@ -16,3 +16,9 @@ curl -L https://gist.github.com/qrush/1595572/raw/6c453ddc959b93895ffbf4fd904e2b
 curl -L -O https://github.com/belluzj/fantasque-sans/releases/download/v1.7.1/FantasqueSansMono.tar.gz
 tar -xvzf FantasqueSansMono.tar.gz
 mv *.ttf ~/.fonts/
+
+
+echo ""
+echo "Setting ZSH as shell"
+echo ""
+sudo chsh -s /usr/bin/zsh $(whoami)
