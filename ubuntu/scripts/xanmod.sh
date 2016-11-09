9d6f45246faff9b0a3b301dbbf44847bab40816a
VJ Patel <meetthevj@gmail.com>
2016-11-09 23:43:00 +0000

    Update xanmod.sh

--- ubuntu/scripts/xanmod.sh
+++ ubuntu/scripts/xanmod.sh
@@ -5,7 +5,7 @@ echo "Installing Xanmod Kernel"
 echo ""
 sudo apt-get install -y curl xz-utils
 
-curl -L https://sourceforge.net/projects/xanmod/files/latest/download -O xanmod.tar.xz
+curl -L https://sourceforge.net/projects/xanmod/files/latest/download -o xanmod.tar.xz
 
 tar -xJf xanmod.tar.xz
 
