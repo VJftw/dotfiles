60c778eee1881bbff05ff33adf91549c12d7215b
VJ Patel <VJftw@users.noreply.github.com>
2017-01-30 20:57:22 +0000

    Update zsh.sh

--- ubuntu/scripts/zsh.sh
+++ ubuntu/scripts/zsh.sh
@@ -9,13 +9,19 @@ git clone --depth 1 https://github.com/zsh-users/antigen.git ~/.antigen
 curl -L https://raw.githubusercontent.com/VJftw/workspace-settings/master/ubuntu/.zshrc -o ~/.zshrc
 
 echo ""
-echo "Installing Fantasque Sans Mono and a Powerline patched font"
+echo "Installing Nerd Fonts and Awesome Terminal Fonts"
 echo ""
 mkdir -p ~/.fonts
 curl -L https://gist.github.com/qrush/1595572/raw/6c453ddc959b93895ffbf4fd904e2ba60256c904/Menlo-Powerline.otf -o ~/.fonts/menlo-powerline.otf
-curl -L -O https://github.com/belluzj/fantasque-sans/releases/download/v1.7.1/FantasqueSansMono.tar.gz
-tar -xvzf FantasqueSansMono.tar.gz
-mv *.ttf ~/.fonts/
+
+curl -L https://github.com/gabrielelana/awesome-terminal-fonts/raw/master/build/devicons-regular.ttf -o ~/.fonts/devicons-regular.ttf
+curl -L https://github.com/gabrielelana/awesome-terminal-fonts/raw/master/build/fontawesome-regular.ttf -o ~/.fonts/fontawesome-regular.ttf
+curl -L https://github.com/gabrielelana/awesome-terminal-fonts/raw/master/build/octicons-regular.ttf -o ~/.fonts/octicons-regular.ttf
+curl -L https://github.com/gabrielelana/awesome-terminal-fonts/raw/master/build/pomicons-regular.ttf -o ~/.fonts/pomicons-regular.ttf
+
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh
 
 
 echo ""
