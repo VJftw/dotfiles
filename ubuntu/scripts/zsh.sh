fdb7a19e4173e2b7697181efc6a1ed6098845427
VJ Patel <vj@vjpatel.me>
2018-08-11 14:57:13 +0100

    downloading releases of fonts for efficiency

--- ubuntu/scripts/zsh.sh
+++ ubuntu/scripts/zsh.sh
@@ -19,10 +19,35 @@ curl -L https://github.com/gabrielelana/awesome-terminal-fonts/raw/master/build/
 curl -L https://github.com/gabrielelana/awesome-terminal-fonts/raw/master/build/octicons-regular.ttf -o ~/.fonts/octicons-regular.ttf
 curl -L https://github.com/gabrielelana/awesome-terminal-fonts/raw/master/build/pomicons-regular.ttf -o ~/.fonts/pomicons-regular.ttf
 
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
-cd nerd-fonts
-./install.sh
-
+# Nerd Fonts
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/3270.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/AnonymousPro.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Arimo.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/FantasqueSansMono.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/FiraCode.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Mono.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hack.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hasklig.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/HeavyData.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hermit.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Inconsolata.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Iosevka.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Lekton.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Meslo.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Monofur.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Monoid.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Mononoki.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/OpenDyslexic.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Overpass.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/ProFont.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/ProggyClean.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/SourceCodePro.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/SpaceMono.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Terminus.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Tinos.zip
+mkdir -p ~/.local/share/fonts/NerdFonts
+unzip -d ~/.local/share/fonts/NerdFonts *.zip
+rm *.zip
 
 echo ""
 echo "Setting ZSH as shell"
