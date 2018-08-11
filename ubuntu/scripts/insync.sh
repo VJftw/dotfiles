b616e987d01772e10d0c14394db0cf1140a03993
VJ Patel <VJftw@users.noreply.github.com>
2018-08-11 15:16:13 +0100

    Update insync.sh

--- ubuntu/scripts/insync.sh
+++ ubuntu/scripts/insync.sh
@@ -4,6 +4,7 @@ echo "Installing Insync"
 echo ""
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-echo 'deb http://apt.insynchq.com/ubuntu xenial non-free contrib' | sudo tee --append /etc/apt/sources.list.d/insync.list
+echo "deb http://apt.insynchq.com/ubuntu $(lsb_release -cs) non-free contrib" | sudo tee --append /etc/apt/sources.list.d/insync.list
+
 sudo apt-get update -y
-sudo apt-get install -y insync
+sudo apt-get install -y insync insync-nautilus
