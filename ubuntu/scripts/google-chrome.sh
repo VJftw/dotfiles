32b8e7e619181ecd4fe3f21ddfd32c0c35706c83
VJ Patel <VJftw@users.noreply.github.com>
2019-02-26 18:21:02 +0000

    Update google-chrome.sh

--- ubuntu/scripts/google-chrome.sh
+++ ubuntu/scripts/google-chrome.sh
@@ -4,5 +4,5 @@ echo ""
 echo "Installing Google Chrome"
 echo ""
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install libappindicator1 -y
+sudo apt-get install libappindicator1 chrome-gnome-shell -y
 sudo dpkg -i google-chrome-stable_current_amd64.deb
