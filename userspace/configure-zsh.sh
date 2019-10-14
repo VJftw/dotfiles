31c40434889d1afeabc19c2e48c47013d53ea4c5
VJ Patel <VJftw@users.noreply.github.com>
2019-10-14 21:25:04 +0100

    Update configure-zsh.sh

--- userspace/configure-zsh.sh
+++ userspace/configure-zsh.sh
@@ -6,6 +6,9 @@ git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # Install powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
+# Install syntax highlighting 
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
 # Download .vjftw.zshrc
 curl -L https://raw.githubusercontent.com/VJftw/dotfiles/master/userspace/home/.vjftw.zshrc -o ~/.vjftw.zshrc
 
