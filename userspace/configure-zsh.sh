707c812bec2f4116e403c5cc5f1107256a6755b8
VJ Patel <VJftw@users.noreply.github.com>
2019-10-14 19:58:22 +0100

    Update configure-zsh.sh

--- userspace/configure-zsh.sh
+++ userspace/configure-zsh.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
-git clone --depth 1 https://github.com/zsh-users/antigen.git ~/.antigen
+
+# Install oh-my-zsh
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# Install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 # Download .vjftw.zshrc
 curl -L https://raw.githubusercontent.com/VJftw/dotfiles/master/userspace/home/.vjftw.zshrc -o ~/.vjftw.zshrc
 
 # source it in zshrc
 grep "source ~/.vjftw.zshrc" ~/.zshrc || echo "source ~/.vjftw.zshrc" >> ~/.zshrc
-
