68eaf9182a9275c252385e1e85f4d16540271b88
VJ Patel <VJftw@users.noreply.github.com>
2019-11-14 16:02:40 +0000

    Update 2-powerlevel9k.theme.zsh

--- userspace/home/.zshrc.d/2-powerlevel9k.theme.zsh
+++ userspace/home/.zshrc.d/2-powerlevel9k.theme.zsh
@@ -4,4 +4,4 @@ POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context dir vcs kubecontext)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
 DEFAULT_USER=$(whoami)
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="powerlevel9k/powerlevel9k"
