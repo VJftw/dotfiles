db1019b2a8d0f22d0fb1e8758649089e3b75e1ba
VJ Patel <VJftw@users.noreply.github.com>
2022-02-11 00:57:42 +0000

    Delete run_once_us-1-kind.sh

--- run_once_us-1-kind.sh
+++
@@ -1,9 +0,0 @@
-{{ if eq .chezmoi.os "linux" -}}
-#!/bin/bash
-set -euo pipefail
-
-curl -L \
-    "https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64" \
-    -o "${HOME}/.local/bin/kind"
-chmod +x "${HOME}/.local/bin/kind"
-{{ end -}}
