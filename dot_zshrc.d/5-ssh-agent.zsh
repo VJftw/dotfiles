c3f44c29e65949921f5e283947f454873c788b59
VJ Patel <VJftw@users.noreply.github.com>
2022-06-02 01:58:29 +0100

    Update 5-ssh-agent.zsh

--- dot_zshrc.d/5-ssh-agent.zsh
+++ dot_zshrc.d/5-ssh-agent.zsh
@@ -1,4 +1,13 @@
 #!/usr/bin/env zsh
 
 eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_ed25519
+
+key_files=(
+  "$HOME/.ssh/id_ed25519"
+)
+
+for key_file in "${key_files[@]}"; do
+  if [ -f "$key_file" ]; then
+    ssh-add "$key_file"
+  fi
+done
