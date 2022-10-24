bf32d553fc7b493d213dba840e03008a53590e96
VJ Patel <vj@vjpatel.me>
2022-10-24 16:22:17 +0100

    update

--- dot_vjftw-ws-setup/kernelspace/docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-curl -fsSL https://get.docker.com | bash
-
-sudo usermod -aG docker $(whoami)
-newgrp docker
--- dot_vjftw-ws-setup/kernelspace/zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-sudo apt-get install -y zsh
-
-sudo chsh -s /usr/bin/zsh $(whoami)
