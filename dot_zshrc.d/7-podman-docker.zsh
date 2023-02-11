37ec5acff3b4eaa9b57e1117922810fee279c4a2
VJ Patel <VJftw@users.noreply.github.com>
2023-02-11 05:49:44 +0000

    Update 7-podman-docker.zsh

--- dot_zshrc.d/7-podman-docker.zsh
+++ dot_zshrc.d/7-podman-docker.zsh
@@ -2,5 +2,5 @@
 
 # This can be enabled by using `systemctl --user enable podman.socket && systemctl --user start podman.socket`
 if [ -v XDG_RUNTIME_DIR ] && [ -S "${XDG_RUNTIME_DIR}/podman/podman.sock" ]; then
-  export DOCKER_HOST="${XDG_RUNTIME_DIR}/podman/podman.sock"
+  export DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
 fi
