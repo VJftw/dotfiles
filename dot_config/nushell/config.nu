98c3aedfce5bbfbf519b36adde82dff7c81c2b92
VJ Patel <vj@vjpatel.me>
2025-05-07 23:59:27 +0000

    feat: add micro text-editor

--- dot_config/nushell/config.nu
+++ dot_config/nushell/config.nu
@@ -1,4 +1,4 @@
 use std/util "path add"
 path add "~/.local/bin"
 
-$env.config.buffer_editor = "vi"
+$env.config.buffer_editor = "micro"
--- run_init.sh
+++ run_init.sh
@@ -10,9 +10,19 @@ mkdir -p "$HOME/third_party/github.com/nushell/nushell/${NUSHELL_VERSION}"
 tar -C "$HOME/third_party/github.com/nushell/nushell/${NUSHELL_VERSION}" -xvzf "nu-${NUSHELL_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 
 mkdir -p "$HOME/.local/bin"
-ln -s "$HOME/third_party/github.com/nushell/nushell/${NUSHELL_VERSION}/nu-${NUSHELL_VERSION}-x86_64-unknown-linux-gnu/nu" "$HOME/.local/bin/nu"
+ln -s "$HOME/third_party/github.com/nushell/nushell/${NUSHELL_VERSION}/nu-${NUSHELL_VERSION}-x86_64-unknown-linux-gnu/nu" "$HOME/.local/bin/nu" || true
 
 # TODO (in nushell):
+# - Micro (text-editor) TODO: migrate to nu.
+mkdir -p "$HOME/third_party/github.com/zyedidia/micro"
+cd "$HOME/third_party/github.com/zyedidia/micro"
+curl https://getmic.ro | bash
+ln -s "$HOME/third_party/github.com/zyedidia/micro/micro" "$HOME/.local/bin/micro" || true
+
+"$HOME/.local/bin/nu"
+
 # - Starship.rs
 # - ASDF-VM
+
+
 
